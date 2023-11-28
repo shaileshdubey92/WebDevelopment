cdd8cb901a1442175b3d3656d559b49001b937c1
shaileshdubey92 <shailesh.dubey92@gmail.com>
2023-11-28 15:03:49 +0530

    day5

---
+++ day5_react/vite-project/src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+
+  const name = 'ram';
+  const name2 = 'syam';
+
+  return (
+    <>
+    <div>
+      <h1>My Name Is {name}</h1>
+      <p>Your name is {name2}</p>
+    </div>
+    <div>This is my div</div>
+
+    </>
+  )
+}
+
+export default App
