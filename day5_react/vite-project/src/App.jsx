f1d1c1f4864a7eb9093cbcf25cd900a357c82a92
shaileshdubey92 <shailesh.dubey92@gmail.com>
2023-12-01 16:30:35 +0530

    day8

--- day5_react/vite-project/src/App.jsx
+++ day5_react/vite-project/src/App.jsx
@@ -4,77 +4,18 @@ import Counter from './Components/Counter';
 import Persondetail from './Components/Persondetail';
 // import Adharcard from './Components/Adharcard';
 import Product from './Components/Product';
+import Navbar from './Components/Navbar';
 
 const App = () => {
-  const products = [
-    {
-      Id: 1,
-      title: 'Apple iPhone 13 (128GB) - Midnight',
-      description:
-        '15 cm (6.1-inch) Super Retina XDR display Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 79999,
-      imgurl:
-        'https://m.media-amazon.com/images/I/61VuVU94RnL._SX679_.jpg',
-      category: 'mobile',
-    },
-    {
-      Id: 2,
-      title: 'Apple iPhone 12 (256GB) - Midnight',
-      description:
-        '12 cm (6.1-inch) Super Retina XDR display Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 89999,
-      imgurl:
-        'https://m.media-amazon.com/images/I/31gmva12KHL._SY445_SX342_QL70_FMwebp_.jpg',
-      category: 'mobile',
-    },
-    {
-      Id: 3,
-      title: 'Fire-Boltt Lumos Stainless Steel',
-      description:
-        '20 cm (6.1-inch) Super Retina XDR disp Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 799,
-      imgurl:
-        'https://m.media-amazon.com/images/I/414qtFAsMDL._SX300_SY300_QL70_FMwebp_.jpg',
-      category: 'smartwatch',
-    },
-    {
-      Id: 4,
-      title: 'Fire-Boltt Lumos Stainless Steel',
-      description:
-        '20 cm (6.1-inch) Super Retina XDR disp Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 9999,
-      imgurl:
-        'https://m.media-amazon.com/images/I/414qtFAsMDL._SX300_SY300_QL70_FMwebp_.jpg',
-      category: 'smartwatch',
-    },
-    {
-      Id: 5,
-      title: 'HP [SmartChoice] 15s, Ryzen 5-5500U, 16GB RAM/512GB SSD 15.6"(39.6 cm)',
-      description:
-        '20 cm (6.1-inch) Super Retina XDR disp Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 59999,
-      imgurl:
-        'https://m.media-amazon.com/images/I/41xo1nI27xL._SX300_SY300_QL70_FMwebp_.jpg',
-      category: 'laptop',
-    },
-
-    {
-      Id: 6,
-      title: 'HP [SmartChoice] 15s, Ryzen 6-5500U, 16GB RAM/512GB SSD 15.6"(39.6 cm)',
-      description:
-        '20 cm (6.1-inch) Super Retina XDR disp Cinematic mode adds shallow depth of field and shifts focus automatically in your videos',
-      price: 89999,
-      imgurl:
-        'https://m.media-amazon.com/images/I/41QAcck4dfL._SX300_SY300_QL70_FMwebp_.jpg',
-      category: 'laptop',
-    },
-  ];
-
+  
   return (
     <>
       <div>
        
-        <Product product={products}/>
+        {/* <data/> */}
+        
+        {/* <Product product={product}/> */}
+        <Product />
       </div>
     </>
   );
--- day5_react/vite-project/src/Components/Product.jsx
+++ day5_react/vite-project/src/Components/Product.jsx
@@ -1,46 +1,66 @@
-import React from 'react'
-const Product = ({product}) => {
-console.log(product);
+import React, { useState } from "react";
+import { product } from "../Product_data";
+
+const Product = () => {
+  const [products, setproducts] = useState(product);
+
+  const filterMethod = (category) =>{
+
+    setproducts(product.filter((data) => data.category == category));
+  }
+
+  // console.log(product);
   return (
     <>
-    <div className='container my-5'>
-    {product.map((data)=>{
-return(
-    <>
-    <div className='text-center my-3'>
-        <h1>{data.title}</h1>
-        <p>{data.description}</p>
-       <div style={{
-        display:"flex",
-        justifyContent:"center",
-        alignItems:"center",
-        margin:"1rem",
-       }}>
-        <img
-        style={{
-            height:"100%",
-            width:"30%",
-            objectFit:"contain",
-        }}
-        src={data.imgurl} alt="img"/>
-        </div>
-        <button className='btn btn-warning'>{data.price}{""}₹</button>
-    </div>
+      <div className="container d-flex justified-content-center sticky-top bg-light">
+        <button
+         onClick={()=>setproducts(product)}
+        className="btn btn-primary mx-3">No_filter</button>
+        <button
+        onClick={()=>filterMethod("mobile")}
+        className="btn btn-warning mx-3">mobile</button>
+        <button
+        onClick={()=>filterMethod("smartwatch")}
+        className="btn btn-danger mx-3">smartwatch</button>
+        <button
+        onClick={()=>filterMethod("laptop")}
+        className="btn btn-secondary mx-3">laptop</button>
+      </div>
+      <div className="container my-5">
+        {products.map((data) => {
+          return (
+            <>
+              <div className="text-center my-5">
+                <h1>{data.title}</h1>
+                <p>{data.description}</p>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    margin: "1rem",
+                  }}
+                >
+                  <img
+                    style={{
+                      height: "100%",
+                      width: "30%",
+                      objectFit: "contain",
+                    }}
+                    src={data.imgurl}
+                    alt="img"
+                  />
+                </div>
+                <button className="btn btn-warning">
+                  {data.price}
+                  {""}₹
+                </button>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </>
-)
-    })
-    }
-    </div>
-    </>
-  )
-}
-export default Product
-
-
-
-
-
-
-
-
-
+  );
+};
+export default Product;
